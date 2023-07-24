e1363e667311843a9eaea7dc3da5b4b32591d7cd
BCupen <bacupen@gmail.com>
2023-07-24 14:45:44 -0400

    firebase inti

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOHVg-IL7dvbPHbjgelrN_bvKjzPxmg5E",
+  authDomain: "whereswaldo-ff708.firebaseapp.com",
+  projectId: "whereswaldo-ff708",
+  storageBucket: "whereswaldo-ff708.appspot.com",
+  messagingSenderId: "521383082979",
+  appId: "1:521383082979:web:a29b5c32c7fbc679937ed6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
